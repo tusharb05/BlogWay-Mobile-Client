562a94f168defbff613f6ee5e273d1eb19ea4f0b
tusharb05 <tusharbaweja2005@gmail.com>
2021-10-07 21:46:21 +0530

    minor

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -43,7 +43,7 @@ export default function homeScreen({ navigation }) {
   };
 
   useEffect(() => {
-    console.log("useEffect triggered");
+    // console.log("useEffect triggered");
     // console.log("useEffect from homeScreen");
     fetchData();
     if (loggedIn) {
