a44691fe61f54fd507675294bfed67fb440c8d5d
tusharb05 <tusharbaweja2005@gmail.com>
2021-10-10 22:50:02 +0530

    final

--- screens/defaultScreen.js
+++ screens/defaultScreen.js
@@ -6,6 +6,8 @@ import homeScreen from "./homeScreen";
 import likedBlogsScreen from "./likedBlogsScreen";
 import addBlogScreen from "./addBlogScreen";
 
+import { AntDesign, Ionicons } from "react-native-vector-icons";
+
 const Tab = createBottomTabNavigator();
 
 export default function defaultScreen() {
@@ -15,24 +17,44 @@ export default function defaultScreen() {
   }, [loggedIn]);
   // console.log(loginDetails);
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+          } else if (route.name === "Liked Blogs") {
+            iconName = focused ? "like1" : "like2";
+          } else if (route.name === "Add Blog") {
+            iconName = focused ? "create" : "create-outline";
+          }
+
+          if (iconName === "like1" || iconName === "like2") {
+            return <AntDesign name={iconName} size={24} color="black" />;
+          } else {
+            return <Ionicons name={iconName} size={24} color="#152D35" />;
+          }
+        },
+      })}
+    >
       <Tab.Screen
         name="Home"
         component={homeScreen}
-        options={{ header: () => null }}
+        options={{ header: () => null, title: "" }}
       />
       {loggedIn && (
         <Tab.Screen
           name="Liked Blogs"
           component={likedBlogsScreen}
-          options={{ header: () => null }}
+          options={{ header: () => null, title: "" }}
         />
       )}
       {loggedIn && (
         <Tab.Screen
           name="Add Blog"
           component={addBlogScreen}
-          options={{ header: () => null }}
+          options={{ header: () => null, title: "" }}
         />
       )}
     </Tab.Navigator>
